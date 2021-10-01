b906f133fb2961c6e6dbec53ef2c81a45023762f
Spandan Dutta <67653554+Spandan-Dutta@users.noreply.github.com>
2021-10-01 19:29:22 +0530

    Update No.1.java

--- String/No.1.java
+++ String/No.1.java
@@ -2,12 +2,12 @@
 //Reverse a String
 
 class Solution {
-    public void reverseString(char[] string) {
-         char [] string1=new char[string.length];
-      string1=string;
-        int c=string.length-1;
-        for(int i=0;i<string.length;i++){
-            string[i]=string1[c];
+    public void reverseString(char[] s) {
+         char [] string1=new char[s.length];
+      string1=s;
+        int c=s.length-1;
+        for(int i=0;i<s.length;i++){
+            s[i]=string1[c];
                 c--;
     }
 }
